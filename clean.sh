2ddf3137266006b316ca366e9e5699d21d8419af
Peter Van Roy <peter.vanroy@uclouvain.be>
2014-12-01 17:10:14 +0100

    Start with all files.

---
+++ clean.sh
@@ -0,0 +1,1 @@
+rm *~ *.backup *.aux *.toc *.blg *.log *.out *.bbl *.dvi *.nav *.pyg *.snm *.lof 2> /dev/null
---
+++ make.sh
@@ -0,0 +1,2 @@
+./clean.sh ; /usr/texbin/pdflatex -shell-escape rapport.tex ; /usr/texbin/pdflatex -shell-escape rapport.tex 
+
