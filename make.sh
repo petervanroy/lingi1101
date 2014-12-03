18a2450a6aef0d62778ca491897f2bfa4886c648
olivierdsh <olivier.desainthubert@student.uclouvain.be>
2014-12-03 14:07:12 +0100

    fixed to make it compile everywhere

--- make.sh
+++ make.sh
@@ -1,2 +1,10 @@
-./clean.sh ; /usr/texbin/pdflatex -shell-escape rapport.tex ; /usr/texbin/pdflatex -shell-escape rapport.tex 
+#!/bin/bash
+./clean.sh
+if [ -x /usr/texbin/pdflatex ]; then
+	/usr/texbin/pdflatex -shell-escape rapport.tex
+	/usr/texbin/pdflatex -shell-escape rapport.tex 
+else
+	pdflatex -shell-escape rapport.tex
+	pdflatex -shell-escape rapport.tex
+fi
 
