baad18e848ef80046c94fb0c6f5e48d6a787fd58
Sue Lambot <sue@lamzi.com>
2014-12-20 15:31:59 +0100

    modif des niveaux de titres

--- down.sh
+++
@@ -1,8 +0,0 @@
-if [ -z "$1" ]; then 
-	echo "please provide the file(s) name";
-else
-	sed -i 's,\\subsubsection,\\paragraph,g' $1;
-	sed -i 's,\\subsection,\\subsubsection,g' $1;
-	sed -i 's,\\section,\\subsection,g' $1;
-	sed -i 's,\\chapter,\\section,g' $1;
-fi
--- up.sh
+++
@@ -1,8 +0,0 @@
-if [ -z "$1" ]; then 
-	echo "please provide the file(s) name";
-else
-	sed -i 's,\\section,\\chapter,g' $1;
-	sed -i 's,\\subsection,\\section,g' $1;
-	sed -i 's,\\subsubsection,\\subsection,g' $1;
-	sed -i 's,\\paragraph,\\subsubsection,g' $1;
-fi
